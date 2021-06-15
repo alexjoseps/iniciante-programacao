8a53f8d4a83ce18e486a7728f14a51a0707ade18
Alex <alexjoseps@outlook.com>
2021-06-14 23:45:15 -0300

    adicionando itens na lista e botão de concluir itens

--- js-na-web/ceep/listaDeTarefas.js
+++ js-na-web/ceep/listaDeTarefas.js
@@ -1,11 +1,40 @@
-const botaoNovaTarefa = document.querySelector('[data-form-button]');
+(() => {
+  const botaoNovaTarefa = document.querySelector('[data-form-button]');
 
-const adicionarNovaTarefa = (event) => {
-  event.preventDefault();
-  const inputNovaTarefa = document.querySelector('[data-form-input]');
-  const conteudo = inputNovaTarefa.value;
-  console.log(conteudo);
-  inputNovaTarefa.value = "";
-};
+  const adicionarNovaTarefa = (event) => {
+    event.preventDefault();
 
-botaoNovaTarefa.addEventListener('click', adicionarNovaTarefa);
+    const inputNovaTarefa = document.querySelector('[data-form-input]');
+    const inputConteudo = inputNovaTarefa.value;
+
+    const listaTarefas = document.querySelector('[data-task-list]');
+    const tarefa = document.createElement('li');
+    const conteudoTarefa = `<p class="content">${inputConteudo}</p>`;
+    tarefa.classList.add('task');
+    tarefa.innerHTML = conteudoTarefa;
+    tarefa.appendChild(BotaoConcluir());
+
+    listaTarefas.appendChild(tarefa);
+
+    inputNovaTarefa.value = "";
+  };
+
+  botaoNovaTarefa.addEventListener('click', adicionarNovaTarefa);
+
+  const BotaoConcluir = () => {
+    const botaoConcluir = document.createElement('button');
+    botaoConcluir.classList.add('check-button');
+    botaoConcluir.innerText = 'concluir';
+
+    botaoConcluir.addEventListener('click', concluirTarefa)
+
+    return botaoConcluir;
+  }
+
+  const concluirTarefa = (event) => {
+    const botaoConcluir = event.target;
+    const tarefa = botaoConcluir.parentElement;
+
+    tarefa.classList.toggle('done');
+  }
+})()
